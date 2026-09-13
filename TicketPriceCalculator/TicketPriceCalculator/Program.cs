7db5d7266a3e131e2bdb8ec545b70dd972ec22f2
Mawuli Bismark <mawulibismark7@gmail.com>
2026-09-13 10:53:09 +0000

    Adding TicketPriceCalculator

---
+++ TicketPriceCalculator/TicketPriceCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 0)
+        {
+            Console.WriteLine("Invalid age.");
+        }
+        else if (age >= 65 || age <= 12)
+        {
+            Console.WriteLine("Ticket Price: GHC 7");
+        }
+        else
+        {
+            Console.WriteLine("Ticket Price: GHC 10");
+        }
+    }
+}
